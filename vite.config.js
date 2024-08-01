f91e74b4fd30cd5a7cf39afeeb74e9afa3d6e3ad
syedaju398@gmail.com <syedaju398@gmail.com>
2024-08-01 14:03:34 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()], 
+  plugins: [react()],
+  server: {
+    open: true,
+  },
   build: {
-    rollupOptions: {
-      external: ['bootstrap/dist/css/bootstrap.css']
-    }
-  }
-})
+    outDir: 'build', // Change this to 'build' to match Vercel's expectation
+  },
+});
